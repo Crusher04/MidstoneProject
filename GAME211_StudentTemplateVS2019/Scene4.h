9f719c0493a016d2662a6e521381dda200dc251b
pnguyen18 <phung.nguyen92@gmail.com>
2023-03-06 12:09:16 -0500

    Level Design + Add 2 prop assets

--- GAME211_StudentTemplateVS2019/Scene4.h
+++ GAME211_StudentTemplateVS2019/Scene4.h
@@ -15,125 +15,60 @@ private:
 	Matrix4 projectionMatrix;	// set in OnCreate()
     Matrix4     inverseProjection;	// set in OnCreate()
 
-	// PHUNG
+	// TEXTURE
 	SDL_Texture* bgTexture;
 	SDL_Texture* pathTexture;
 
-	//tree row 1
+	//tree
 	SDL_Texture* treeTexture0;
 	SDL_Texture* treeTexture1;
-	SDL_Texture* treeTexture2;
-	SDL_Texture* treeTexture3;
-	SDL_Texture* treeTexture4;
-	SDL_Texture* treeTexture5;
-	SDL_Texture* treeTexture6;
-	SDL_Texture* treeTexture7;
+	
+	//other Texture
+	SDL_Texture* wellTexture;
+	SDL_Texture* otherTexture0;
+	SDL_Texture* otherTexture1;
+	SDL_Texture* logTexture0;
 
-	//tree row 1.1
-	SDL_Texture* treeTexture0_1;
-	SDL_Texture* treeTexture1_1;
-	SDL_Texture* treeTexture2_1;
-	SDL_Texture* treeTexture3_1;
-	SDL_Texture* treeTexture4_1;
-	SDL_Texture* treeTexture5_1;
-	SDL_Texture* treeTexture6_1;
-	SDL_Texture* treeTexture7_1;
-
-	//tree row 2
-	SDL_Texture* treeTexture0_2;
-	SDL_Texture* treeTexture1_2;
-	SDL_Texture* treeTexture2_2;
-	SDL_Texture* treeTexture3_2;
-	SDL_Texture* treeTexture4_2;
-	SDL_Texture* treeTexture5_2;
-	SDL_Texture* treeTexture6_2;
-	SDL_Texture* treeTexture7_2;
-
-	//tree row 3
-	SDL_Texture* treeTexture0_3;
-	SDL_Texture* treeTexture1_3;
-	SDL_Texture* treeTexture2_3;
-	SDL_Texture* treeTexture3_3;
-	SDL_Texture* treeTexture4_3;
-	SDL_Texture* treeTexture5_3;
-	SDL_Texture* treeTexture6_3;
-	SDL_Texture* treeTexture7_3;
-
-	//tree row 4
-	SDL_Texture* treeTexture0_4;
-	SDL_Texture* treeTexture1_4;
-	SDL_Texture* treeTexture2_4;
-	SDL_Texture* treeTexture3_4;
-	SDL_Texture* treeTexture4_4;
-	SDL_Texture* treeTexture5_4;
-	SDL_Texture* treeTexture6_4;
-	SDL_Texture* treeTexture7_4;
-
-	//other
-	SDL_Texture* stumpTexture;
 
 	//block
 	SDL_Texture* blockTexture0;
 	SDL_Texture* blockTexture1;
+	SDL_Texture* blockTexture2;
+	SDL_Texture* blockTexture3;
 
+	//rock
+	SDL_Texture* rockTexture0;
+	SDL_Texture* rockTexture1;
+	SDL_Texture* rockTexture2;
+	SDL_Texture* rockTexture3;
+	
+	
+	//_____________________________________IMAGE
 	Body* bg;
 
 	SDL_Surface* bgImage;
 	SDL_Surface* pathImage;
 
-	//tree row 1
+	//tree
 	SDL_Surface* treeImage0;
 	SDL_Surface* treeImage1;
-	SDL_Surface* treeImage2;
-	SDL_Surface* treeImage3;
-	SDL_Surface* treeImage4;
-	SDL_Surface* treeImage5;
-	SDL_Surface* treeImage6;
-	SDL_Surface* treeImage7;
-
-	//tree row 1.1
-	SDL_Surface* treeImage0_1;
-	SDL_Surface* treeImage1_1;
-	SDL_Surface* treeImage2_1;
-	SDL_Surface* treeImage3_1;
-	SDL_Surface* treeImage4_1;
-	SDL_Surface* treeImage5_1;
-	SDL_Surface* treeImage6_1;
-	SDL_Surface* treeImage7_1;
-
-	//tree row 2
-	SDL_Surface* treeImage0_2;
-	SDL_Surface* treeImage1_2;
-	SDL_Surface* treeImage2_2;
-	SDL_Surface* treeImage3_2;
-	SDL_Surface* treeImage4_2;
-	SDL_Surface* treeImage5_2;
-	SDL_Surface* treeImage6_2;
-	SDL_Surface* treeImage7_2;
-
-	//tree row 3
-	SDL_Surface* treeImage0_3;
-	SDL_Surface* treeImage1_3;
-	SDL_Surface* treeImage2_3;
-	SDL_Surface* treeImage3_3;
-	SDL_Surface* treeImage4_3;
-	SDL_Surface* treeImage5_3;
-	SDL_Surface* treeImage6_3;
-	SDL_Surface* treeImage7_3;
-
-	//tree row 4
-	SDL_Surface* treeImage0_4;
-	SDL_Surface* treeImage1_4;
-	SDL_Surface* treeImage2_4;
-	SDL_Surface* treeImage3_4;
-	SDL_Surface* treeImage4_4;
-	SDL_Surface* treeImage5_4;
-	SDL_Surface* treeImage6_4;
-	SDL_Surface* treeImage7_4;
 
 	//block
 	SDL_Surface* blockImage0;
 	SDL_Surface* blockImage1;
+	SDL_Surface* blockImage2;
+
+	//other
+	SDL_Surface* wellImage;
+	SDL_Surface* otherImage0;
+	SDL_Surface* otherImage1;
+	SDL_Surface* logImage0;
+
+	//rock
+	SDL_Surface* rockImage0;
+	SDL_Surface* rockImage1;
+	SDL_Surface* rockImage2;
+
 
 	SDL_Texture* loadImage(const char* textureFile);
 
