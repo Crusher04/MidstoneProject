cc95d28c6720139d5eb0fe7a28367f44874b0d16
Crusher04 <35314898+Crusher04@users.noreply.github.com>
2023-03-28 17:05:32 -0400

    working on zombie spawning

--- GAME211_StudentTemplateVS2019/ZombieSpawner.cpp
+++ GAME211_StudentTemplateVS2019/ZombieSpawner.cpp
@@ -97,6 +97,9 @@ REGZOMBIE:
 		}
 		health.setHealth(50);
         regZombCounter--;
+        std::srand((unsigned int)time(NULL));
+        float randSpeed = (rand() % 2) + 1;
+
 	}
  
     return true;
